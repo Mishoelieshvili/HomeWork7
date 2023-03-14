b2c2c8392d3e8cf494184c81d11ca9489a0d5e9e
Misho <126102456+Mishoelieshvili@users.noreply.github.com>
2023-03-14 16:30:10 +0400

    Tasks7

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine($"Task 1");
+        Console.WriteLine("...........\n");
+        JackpotFound();
+        Console.WriteLine("\n...........");
+
+        Console.WriteLine($"Task 2");
+        Console.WriteLine("...........\n");
+        int totalPoints = CalculatePoints(3, 1, 1);
+        Console.WriteLine("Total points: " + totalPoints);
+        Console.WriteLine("\n...........");
+
+        Console.WriteLine($"Task 3");
+        Console.WriteLine("...........\n");
+        Performances();
+        Console.WriteLine("\n...........");
+
+        Console.WriteLine($"Task 4");
+        Console.WriteLine("...........\n");
+        string[] arr = { "Hello", "World", "Programming", "communication" };
+        int n = 7;
+        PrintElementOfLengthN(arr, n);
+        Console.WriteLine("\n...........");
+    }
+
+    static void JackpotFound()
+    {
+        Console.Write("Enter jackpot characters: ");
+        string[] input = Console.ReadLine().Split();
+
+        // Check if all characters are the same
+        bool jackpotFound = input.Length > 0 && input.All(c => c == input[0] && !string.IsNullOrWhiteSpace(c));
+
+        if (jackpotFound)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+
+    public static int CalculatePoints(int wins, int draws, int losses)
+    {
+        Console.Write("Enter the Wins, Draws, Losses: ");
+        string[] input = Console.ReadLine().Split();
+
+        int[] performances = Array.ConvertAll(input, int.Parse);
+        int totalPoints = (wins * 3) + (draws * 1) + (losses * 0);
+
+        return totalPoints;
+    }
+
+    static void Performances()
+    { 
+                Console.Write("Enter the daily performances separated by commas: ");
+                string[] input = Console.ReadLine().Split(',');
+
+                int[] performances = Array.ConvertAll(input, int.Parse);
+                int count = 0;
+       
+                for (int i = 1; i < performances.Length; i++)
+                {
+                    if (performances[i] > performances[i - 1])
+                    {
+                        count++;
+                    }
+                }
+
+         Console.WriteLine("The number of days Giorgi improved his performance is: " + count);
+    }
+
+    public static void PrintElementOfLengthN(string[] arr, int n)
+    {
+        Console.Write("Enter the Element: ");
+        string[] input = Console.ReadLine().Split();
+
+        string result = "";
+        foreach (string s in arr)
+        {
+            if (s.Length == n)
+            {
+                result += s + ", ";
+            }
+        }
+
+        if (result.Length > 0)
+        {
+            Console.WriteLine(result.TrimEnd(',', ' '));
+        }
+        else
+        {
+            Console.WriteLine("No elements found");
+        }
+     
+    }
+
+}
+
